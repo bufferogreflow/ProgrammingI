f50bcd335c0398b1733c8210aa66d4b13afc4f73
bufferogreflow <angelortornello@gmail.com>
2019-01-04 09:31:32 +0000

    Added comments to solution 3

--- Chapter2/solution3.py
+++ Chapter2/solution3.py
@@ -1,2 +1,2 @@
 name = input("Name: ")
-print('Hello {0}, would you like to learn some Python today?'.format(name))
+print('Hello {0}, would you like to learn some Python today?'.format(name)) #strings have a format method for displaying var values in them
